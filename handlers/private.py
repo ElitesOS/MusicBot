c5b9c77e3bdb362460d06e2602e1238c27942de9
Simple Boy <82438251+ElitesOS@users.noreply.github.com>
2022-02-24 16:03:55 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -10,25 +10,25 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/c70635cc28eec97a43f40.jpg",
+        photo=f"https://te.legra.ph/file/12514ab36b0869139b77a.jpg",
         caption=f"""**I Am A Smooth Telegram Music Bot With Fantastic Features & High Quality Songs**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Created By", url=f"https://t.me/TeamB2k")
+                        "[Created By]", url=f"https://t.me/TeamB2k")
                ],
                 [
                     InlineKeyboardButton(
-                        "Repository", url=f"https://github.com/ElitesOS/MusicBot")
+                        "[Repository]", url=f"https://github.com/ElitesOS/MusicBot")
                ], 
                 [
                     InlineKeyboardButton(
-                        "Support Chat", url=f"https://t.me/TeamB2kSupport")
+                        "[Support Chat]", url=f"https://t.me/TeamB2kSupport")
                ],
                 [
                     InlineKeyboardButton(
-                        "Help [24×7]", url=f"https://t.me/TeamB2kSupport")
+                        "[Help] [24×7]", url=f"https://t.me/TeamB2kSupport")
                 ]
                 
            ]
@@ -38,7 +38,7 @@ async def start_(client: Client, message: Message):
 @Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/11a149401351a322dc1bc.jpg",
+        photo=f"https://te.legra.ph/file/12514ab36b0869139b77a.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
